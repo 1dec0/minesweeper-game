d87886898b94e1142f1ed8770eecebcd651841a6
Madison K <decness@hotmail.com>
2020-06-02 18:53:21 -0700

    Basic Outlining of Canvas and Tile

--- Project1/Project1/Canvas.h
+++ Project1/Project1/Canvas.h
@@ -1,12 +1,45 @@
 #ifndef CANVAS_H
 #define CANVAS_H
 
+#include <iostream>
+
+class Tile
+{
+public:
+	Tile();
+
+	void setMarked(int newValue);
+	int getValue();
+
+	ostream& operator<<(ostream& out);
+
+private:
+	int x;
+	int y;
+	int value;
+	int markedValue;
+};
+
 class Canvas
 {
 public:
+	Canvas();
+	Canvas(int numOfBombs);
+	Canvas(int width, int length);
+	Canvas(int width, int length, int numOfBombs);
+	Canvas(const Canvas&);
+
+	Canvas& operator=(const Canvas&); //assignment operator
+
+	~Canvas();
 
 private:
+	int width;
+	int length;
 
+	int ** board; //Should this be Tile ** board;?
+
+	int numOfBombs;
 };
 
 
